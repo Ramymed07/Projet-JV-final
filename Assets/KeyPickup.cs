d2822f2b4f64933fbeb9f0a4606d1256c2fc4087
emilieqo <emilie.qo@icloud.com>
2026-04-10 00:24:39 -0400

    -

--- Assets/KeyPickup.cs
+++ Assets/KeyPickup.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class KeyPick : MonoBehaviour
+public class KeyPickup : MonoBehaviour
 {
     [Header("Key Settings")]
     public string lockedDoorTag1 = "locked door A";
